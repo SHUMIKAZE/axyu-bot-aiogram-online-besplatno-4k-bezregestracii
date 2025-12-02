786403eeed0011c1549f3685af52515bb65db031
SHUMIKAZE <shumikaze@proton.me>
2025-12-02 22:32:15 +0300

    blya axyu a ne bot

---
+++ main.py
@@ -0,0 +1,42 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+# 1. Вставь сюда свой токен
+TOKEN = '7062578652:AAGE0rZScE6mZYmXvxWuTjwL5yvGvdnRN_c'
+
+# Включаем логирование, чтобы видеть в консоли, что происходит
+logging.basicConfig(level=logging.INFO)
+
+# 2. Создаем объекты бота и диспетчера
+# Диспетчер (dp) - это "уши" бота, он слушает сообщения
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+# 3. Обработчик команды /start
+# Когда пользователь нажмет Start, сработает эта функция
+@dp.message(CommandStart())
+async def cmd_start(message: types.Message):
+    # message.from_user.first_name - это имя того, кто написал
+    await message.answer(f"Привет, {message.from_user.first_name}!\nХа, лох!! ты не {message.from_user.first_name}! Ты долбаеб!!")
+
+# 4. Обработчик любого текста (Эхо)
+# Сработает, если это не команда, а просто текст
+@dp.message()
+async def echo_answer(message: types.Message):
+    # Мы просто отправляем тот же текст обратно
+    await message.reply(f"В смысле '{message.text}'? Ты охуел?")
+
+# 5. Функция запуска
+async def main():
+    print("Бот не запущен!")
+    # start_polling - бот постоянно спрашивает у Telegram: "Есть новые сообщения?"
+    await dp.start_polling(bot)
+
+# Эта конструкция запускает код, только если мы запускаем файл напрямую
+if name == "main":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Бот вЫЫЫЫключен")
