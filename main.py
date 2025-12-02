24b949d069ed00d084ebe5e1dc7be57c826c3304
SHUMIKAZE <shumikaze@proton.me>
2025-12-02 22:39:49 +0300

    бля ощибочка вышла, ща все норм, не ссы

--- main.py
+++ main.py
@@ -19,24 +19,24 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def cmd_start(message: types.Message):
     # message.from_user.first_name - это имя того, кто написал
-    await message.answer(f"Привет, {message.from_user.first_name}!\nХа, лох!! ты не {message.from_user.first_name}! Ты долбаеб!!")
+    await message.answer(f"Привет, {message.from_user.first_name}!\n\n Ха, лох ебаный! Ты не {message.from_user.first_name}, ты долбаеб!")
 
 # 4. Обработчик любого текста (Эхо)
 # Сработает, если это не команда, а просто текст
 @dp.message()
 async def echo_answer(message: types.Message):
     # Мы просто отправляем тот же текст обратно
-    await message.reply(f"В смысле '{message.text}'? Ты охуел?")
+    await message.answer(f"В смысле блять '{message.text}'? Ты охуел?")
 
 # 5. Функция запуска
 async def main():
-    print("Бот не запущен!")
+    print("Бот запущен!")
     # start_polling - бот постоянно спрашивает у Telegram: "Есть новые сообщения?"
     await dp.start_polling(bot)
 
 # Эта конструкция запускает код, только если мы запускаем файл напрямую
-if name == "main":
+if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("Бот вЫЫЫЫключен")
+        print("Бот выключен")
